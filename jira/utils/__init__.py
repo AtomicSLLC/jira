60867d1b3f5208ed74dc62ed2dad5cbf4d20ba97
Arthur Sahakyan <artursahakyan1996@gmail.com>
2020-12-08 13:48:17 +0400

    Update __init__.py

--- jira/utils/__init__.py
+++ jira/utils/__init__.py
@@ -36,14 +36,11 @@ class CaseInsensitiveDict(dict):
     def __init__(self, *args, **kw):
         super(CaseInsensitiveDict, self).__init__(*args, **kw)
 
-         upper_keys_list = []
-         for key in super(CaseInsensitiveDict, self).keys():
-             if key != key.lower():
-                 upper_keys_list.append(key)
-
-         for upper_key in upper_keys_list:
-             self[upper_key.lower()] = self[upper_key]
-             self.pop(upper_key, None)
+        self.itemlist = {}
+        for key, value in super(CaseInsensitiveDict, self).copy().items():
+         if key != key.lower():
+             self[key.lower()] = value
+             self.pop(key, None)
 
         # self.itemlist[key.lower()] = value
 
